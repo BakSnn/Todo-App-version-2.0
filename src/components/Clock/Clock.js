6537cb4852babab317d4e1d2a5ea6ccf4592e49a
BakSnn <baskarev.nikita@mail.ru>
2023-08-03 13:59:32 +0300

    Added a Clock

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,26 @@
+﻿import { useEffect, useState } from 'react'
+import styles from './Clock.module.css'
+
+function Clock() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  const formatedTime = time.toLocaleTimeString()
+
+  return (
+    <div >
+      <h1 className={styles.clock}>{formatedTime}</h1>
+    </div>
+  )
+}
+
+export default Clock
