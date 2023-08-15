6c71d383d05619b335297577638cb4c1ad164cbb
BakSnn <baskarev.nikita@mail.ru>
2023-08-15 22:56:39 +0300

    casd

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import styles from './TodoForm.module.css'
-import Button from '../UI/Button/Button'
+import Button from '../UI/Button/'
 function TodoForm({ addTodo }) {
   const [text, setText] = useState('')
 
--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -1,4 +1,4 @@
-﻿import Button from '../UI/Button/Button'
+﻿import Button from '../UI/Button/'
 import { RiDeleteBin2Line, RiRefreshLine } from 'react-icons/ri'
 
 function TodosActions({
