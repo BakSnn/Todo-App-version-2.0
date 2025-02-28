bd40899b7f96d0df90d6b87f5d14c42b5b80e2e3
BakSnn <112386046+BakSnn@users.noreply.github.com>
2025-02-28 21:06:51 +0300

    added created date for todo

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,14 +1,23 @@
-import { RiTodoFill } from 'react-icons/ri'
-import { BsTrashFill } from 'react-icons/bs'
-import { ImCheckmark } from 'react-icons/im'
-import styles from './Todo.module.css'
+import { RiTodoFill } from "react-icons/ri";
+import { BsTrashFill } from "react-icons/bs";
+import { ImCheckmark } from "react-icons/im";
+import { IoCreateOutline } from "react-icons/io5";
+
+import styles from "./Todo.module.css";
+import { useState, useEffect } from "react";
 function Todo({ todo, deleteTodo, toggleTodo }) {
+  const [createdDate, setCreatedDate] = useState(
+    new Date().toLocaleDateString()
+  );
+
   return (
     <div
       className={`${styles.todo} ${
-        todo.isCompleted ? styles.completedTodo : ''
+        todo.isCompleted ? styles.completedTodo : ""
       }`}
     >
+      <IoCreateOutline className={styles.createdDateIcon}></IoCreateOutline>
+      <span className={styles.date}>{createdDate.slice(0,5)}</span>
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
       <BsTrashFill
@@ -20,6 +29,6 @@ function Todo({ todo, deleteTodo, toggleTodo }) {
         onClick={() => toggleTodo(todo.id)}
       />
     </div>
-  )
+  );
 }
-export default Todo
+export default Todo;
